b1b7ff978a794131287c3d4e33f25ca8282a5117
Juan Pablo Leyva <jpleyvat@gmail.com>
2020-01-31 19:12:45 -0500

    Autoplay plugiin.

--- assets/index.js
+++ assets/index.js
@@ -1,18 +1,17 @@
+import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
+
 const video = document.querySelector('video');
-const button = document.querySelector('button');
 
-function MediaPlayer(config) {
-	this.media = config.el;
-}
+const player = new MediaPlayer({
+	el: video,
+	plugins: [
+		// new AutoPlay()
+	]
+});
 
-MediaPlayer.prototype.play = function() {
-	this.media.play();
-};
+const playButton = document.querySelector('button');
+const muteButton = document.getElementById('mute');
 
-MediaPlayer.prototype.pause = function() {
-	this.media.pause();
-};
-
-const player = new MediaPlayer({ el: video });
-
-button.onclick = () => player.play();
+playButton.onclick = () => player.togglePlay();
+muteButton.onclick = () => player.toggleMute();
