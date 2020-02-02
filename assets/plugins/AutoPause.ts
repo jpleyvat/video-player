91cefddff1a047e58da1e624dc93f7709fe8da6d
Juan Pablo Leyva <jpleyvat@gmail.com>
2020-02-02 16:43:31 -0500

    Typescript migration.

--- assets/plugins/AutoPause.ts
+++ assets/plugins/AutoPause.ts
@@ -1,9 +1,17 @@
+import MediaPlayer from '../MediaPlayer.js';
+
 class AutoPause {
+	private threshold: number;
+	player: MediaPlayer;
+
 	constructor() {
 		this.threshold = 0.25;
 		this.handleIntersection = this.handleIntersection.bind(this);
+		this.handleVisibilityChange = this.handleVisibilityChange.bind(
+			this
+		);
 	}
-	run(player) {
+	run(player: MediaPlayer) {
 		this.player = player;
 
 		const observer = new IntersectionObserver(
@@ -14,15 +22,29 @@ class AutoPause {
 		);
 
 		observer.observe(this.player.media);
+
+		document.addEventListener(
+			'visibilitychange',
+			this.handleVisibilityChange
+		);
 	}
 
-	handleIntersection(entries) {
+	private handleIntersection(entries: IntersectionObserverEntry[]) {
 		const entry = entries[0];
 
 		const isVisible = entry.intersectionRatio >= this.threshold;
 
 		if (isVisible) {
 			// console.log('visible');
+			this.player.play();
+		} else {
+			this.player.pause();
+		}
+	}
+
+	private handleVisibilityChange() {
+		const isVisible = document.visibilityState === 'visible';
+		if (isVisible) {
 			this.player.play();
 		} else {
 			this.player.pause();
